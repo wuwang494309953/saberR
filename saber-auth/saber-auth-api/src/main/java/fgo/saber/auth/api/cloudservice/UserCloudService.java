7471b1c44185cb6e0b149ea435ac45664e59448e
ZQ <494309953@qq.com>
2018-09-13 23:16:37 +0800

    提交一下下

--- saber-auth/saber-auth-api/src/main/java/fgo/saber/auth/api/cloudservice/UserCloudService.java
+++ saber-auth/saber-auth-api/src/main/java/fgo/saber/auth/api/cloudservice/UserCloudService.java
@@ -1,5 +1,11 @@
 package fgo.saber.auth.api.cloudservice;
 
+import fgo.saber.auth.api.dto.UserDto;
+import fgo.saber.base.json.JsonResult;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.constraints.NotNull;
+
 /**
  * @author zq
  * @Date 2018/9/13
@@ -7,5 +13,8 @@ package fgo.saber.auth.api.cloudservice;
 public interface UserCloudService {
 
 
+    JsonResult<UserDto> findUserWithId(@NotNull @RequestParam(name = "userId") Long userId);
+
+    JsonResult<Boolean> saveUser(@NotNull @RequestParam(name = "userId") UserDto userDto);
 
 }
