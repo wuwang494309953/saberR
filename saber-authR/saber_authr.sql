e0153baf23156737b4f35bc0429b98e2384f96f4
Saber <494309953@qq.com>
2019-12-24 19:50:28 +0800

    docker化

--- saber-authR/saber_authr.sql
+++ saber-authR/saber_authr.sql
@@ -12,8 +12,8 @@ File Encoding         : 65001
 
 Date: 2019-09-25 20:27:59
 */
-
-create database saber_authr CHARACTER SET utf8 COLLATE utf8_general_ci;
+SET NAMES 'utf8mb4';
+create database saber_authr CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 use saber_authr;
 
